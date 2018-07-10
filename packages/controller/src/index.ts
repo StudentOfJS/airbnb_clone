a795620c4616a78ed37817e502ed7dfb73dec420
StudentOfJS <rodlewis45@gmail.com>
2018-07-10 13:49:08 +0200

    export NormailizedErrorMap

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -2,3 +2,4 @@ export * from "./modules/RegisterController";
 export * from "./schemaTypes";
 export * from "./modules/ForgotPasswordController";
 export * from "./modules/LoginController";
+export * from "./modules/types/NormailizedErrorMap";
