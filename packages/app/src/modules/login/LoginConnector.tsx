902c715b97ed315c157e40e0ce115e598881061b
StudentOfJS <rodlewis45@gmail.com>
2018-07-10 10:22:58 +0200

    store sessionId expo

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { LoginController } from "@airbnb_clone/controller";
 import { LoginView } from "./ui/LoginView";
+import { SecureStore } from "expo";
 
 export class LoginConnector extends React.PureComponent {
+  saveSessionId = (sid: string) => {
+    SecureStore.setItemAsync("sid", sid);
+  };
+
   render() {
     return (
-      <LoginController>
+      <LoginController onSessionId={this.saveSessionId}>
         {({ submit }) => <LoginView submit={submit} />}
       </LoginController>
     );
