862055e13bf23d1d58171e51215bb486db40da80
StudentOfJS <rodlewis45@gmail.com>
2018-07-11 10:19:29 +0200

    redirect after login

--- packages/web/src/modules/login/LoginConnector.tsx
+++ packages/web/src/modules/login/LoginConnector.tsx
@@ -2,11 +2,17 @@ import * as React from "react";
 
 import { LoginView } from "./ui/LoginView";
 import { LoginController } from "@airbnb_clone/controller";
+import { RouteComponentProps } from "react-router-dom";
 
-const LoginConnector: React.SFC = () => (
-  <LoginController>
-    {({ submit }: { submit: any }) => <LoginView submit={submit} />}
-  </LoginController>
-);
+const LoginConnector: React.SFC<RouteComponentProps<{}>> = ({ history }) => {
+  const onFinish = () => history.push("/");
+  return (
+    <LoginController>
+      {({ submit }: { submit: any }) => (
+        <LoginView submit={submit} onFinish={onFinish} />
+      )}
+    </LoginController>
+  );
+};
 
 export default LoginConnector;
