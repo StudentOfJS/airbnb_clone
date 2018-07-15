875cf59163a58a301da95e853b9fc8061bd0aea2
StudentOfJS <rodlewis45@gmail.com>
2018-07-15 16:38:44 +0200

    check for session id

--- packages/server/src/modules/listing/create/resolvers.ts
+++ packages/server/src/modules/listing/create/resolvers.ts
@@ -4,6 +4,9 @@ import { Listing } from "../../../entity/Listing";
 export const resolvers: ResolverMap = {
   Mutation: {
     createListing: async (_, { input }, { session }) => {
+      if(!session.userId) {
+        throw new Error("not authenticated")
+      }
       await Listing.create({
         ...input,
         pictureUrl: "",
