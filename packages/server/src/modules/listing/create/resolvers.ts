33863589facfc1f29a135b3994251072ae716632
StudentOfJS <rodlewis45@gmail.com>
2018-07-24 17:20:19 +0200

    file upload resolver

--- packages/server/src/modules/listing/create/resolvers.ts
+++ packages/server/src/modules/listing/create/resolvers.ts
@@ -1,15 +1,38 @@
 import { ResolverMap } from "../../../types/graphql-utils";
 import { Listing } from "../../../entity/Listing";
+import * as shortid from "shortid";
+import { createWriteStream } from "fs";
+
+const storeUpload = async ({ stream }: any): Promise<any> => {
+  const id = shortid.generate();
+  const path = `images/${id}`;
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on("finish", () => resolve({ id, path }))
+      .on("error", reject)
+  );
+};
+
+const processUpload = async (upload: any) => {
+  const { stream, filename } = await upload;
+  const { id } = await storeUpload({ stream, filename });
+  return id;
+};
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    createListing: async (_, { input }, { session }) => {
-      if(!session.userId) {
-        throw new Error("not authenticated")
+    createListing: async (_, { input: { picture, ...data } }, { session }) => {
+      if (!session.userId) {
+        throw new Error("not authenticated");
       }
+
+      const pictureUrl = await processUpload(picture);
+
       await Listing.create({
-        ...input,
-        pictureUrl: "",
+        ...data,
+        pictureUrl,
         userId: session.userId
       }).save();
       return true;
